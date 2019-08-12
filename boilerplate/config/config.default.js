c179c9cec979c3445e8b2ad8967305f0c2285c01
unclexiao <xh449129722@gmail.com>
2019-08-12 17:19:50 +0800

    feat: add framework and plugins

--- boilerplate/config/config.default.js
+++ boilerplate/config/config.default.js
@@ -12,15 +12,43 @@ module.exports = appInfo => {
    **/
   const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
+  // 加密字符串
   config.keys = appInfo.name + '_{{keys}}';
 
-  // add your middleware config here
+  // 微信小程序
+  config.mp = {
+    appId: '', // 公众平台应用编号
+    appSecret: '', // 公众平台应用密钥
+    mchId: '', // 商户平台商家编号
+    apiKey: '', // 商户支付密钥
+  };
+
+  // 高性能缓存
+  config.redis = {
+    client: {
+      host: process.env.EGG_REDIS_HOST || '127.0.0.1', // 主机
+      port: process.env.EGG_REDIS_PORT || 6379, // 端口
+      password: process.env.EGG_REDIS_PASSWORD || '', // 密码
+      db: process.env.EGG_REDIS_DB || '0', // 数据库
+    },
+  };
+
+  // 文档型数据库
+  config.mongoose = {
+    url: 'mongodb://localhost:27017/teller', // 地址
+    options: {
+      useNewUrlParser: true,
+      autoIndex: true,
+      useCreateIndex: true,
+    },
+  };
+
+  // 中间件
   config.middleware = [];
 
-  // add your user config here
+  // 用户配置
   const userConfig = {
-    // myAppName: 'egg',
+    myAppName: 'tell-mp',
   };
 
   return {
